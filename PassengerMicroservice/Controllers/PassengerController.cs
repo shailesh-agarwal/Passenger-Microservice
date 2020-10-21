df4a30c3f1157975e2cab12dd9c8a323981495ad
shailesh-agarwal <shailesh.ag.20@gmail.com>
2020-10-21 18:15:42 +0530

    first-commit

---
+++ PassengerMicroservice/Controllers/PassengerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PassengerMicroservice.Repositories;
+
+namespace PassengerMicroservice.Controllers
+{
+    [ApiController]
+    public class PassengerController : ControllerBase
+    {
+        ipassenger pass;
+
+        public PassengerController(ipassenger _pass)
+        {
+            pass = _pass;
+        }
+
+        [HttpGet]
+        [Route("api/Passenger/GetTicketType")]
+        public IActionResult GetTicketType()
+        {
+            try
+            {
+                var tickets = pass.GetTicketType();
+
+                if (tickets != null)
+                {
+                    return Ok(tickets);
+                }
+
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Passenger/GetBookingDetail")]
+        public IActionResult GetBookingDetail(int? id)
+        {
+            try
+            {
+                var booking = pass.GetBookingDetail(id);
+
+                if (booking != null)
+                {
+                    return Ok(booking);
+                }
+
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ PassengerMicroservice/Repositories/ipassenger.cs
@@ -0,0 +1,14 @@
+﻿using PassengerMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassengerMicroservice.Repositories
+{
+    public interface ipassenger
+    {
+        List<TicketType> GetTicketType();
+        List<BookingDetail> GetBookingDetail(int? id);
+    }
+}
